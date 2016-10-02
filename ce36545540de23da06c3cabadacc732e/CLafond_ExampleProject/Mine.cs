dac64c371cf52ac14aa44c5d55546ff3ead12c4f
Christopher J. Lafond <lafond.chris@yahoo.com>
2016-10-01 22:41:49 -0400

    Application Submission for Christopher J Lafond
    Application Submission for Christopher J Lafond

---
+++ ce36545540de23da06c3cabadacc732e/CLafond_ExampleProject/Mine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GD327_Final_Project
+{
+    public class Mine : Sprite
+    {
+
+
+        private PowerUp _powerup;
+
+        public PowerUp PowerUp
+        {
+            get { return _powerup; }
+            set
+            {
+                _powerup = value;
+                switch (this.PowerUp)
+                {
+                    case PowerUp.Shield:
+                        this.Color = Color.Red;
+                        break;
+                    case PowerUp.GunUpgrade:
+                        this.Color = Color.Cyan;
+                        break;
+                    case PowerUp.Empty:
+                        this.Color = Color.White;
+                        break;
+                }
+            }
+        }
+
+
+        public MineController.MineType Type { get; private set; }
+        public Mine(Game game, Vector2 position, SpriteBatch batch, MineController.MineType type, float scale = 1, float opacity = 1, float rotation = 0, float rotationPerUpdate = 0)
+            : base(game, type == MineController.MineType.Big ? MineController.BigMineTexture : MineController.SmallMineTexture, position, batch, scale, opacity, rotation, rotationPerUpdate)
+        {
+            Type = type;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            switch (PowerUp)
+            {
+                case PowerUp.Shield:
+                    SpriteBatch.DrawString(TextController.Font, "Shield", Position - Vector2.One * 20, Color.White);
+                    break;
+                case PowerUp.GunUpgrade:
+                    SpriteBatch.DrawString(TextController.Font, "Guns", Position - Vector2.One * 20, Color.White);
+                    break;
+            }
+
+        }
+
+    }
+}
---
+++ ce36545540de23da06c3cabadacc732e/CLafond_ExampleProject/TextSprite.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace GD327_Final_Project
+{
+    public class TextSprite : Sprite
+    {
+        public float MsLifeLeft { get; set; }
+        public float OriginalLifeInMiliseconds { get; set; }
+        public float BeginScale { get; set; }
+        public float EndScale { get; set; }
+
+        float _scaleGrowthPerMilisecond;
+
+        internal TextSprite(Game game, Texture2D texture, Vector2 position, SpriteBatch spritebatch, Color color, float beginScale = 1, float endScale = 1, float msLifeLength = 400)
+            : base(game, texture, position, spritebatch)
+        {
+            OriginalLifeInMiliseconds = msLifeLength;
+            BeginScale = beginScale;
+            EndScale = endScale;
+            Color = color;
+            Reset();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            MsLifeLeft -= (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            base.Update(gameTime);
+            Scale += (float)gameTime.ElapsedGameTime.TotalMilliseconds * _scaleGrowthPerMilisecond;
+            Position -= Vector2.One * 70 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (MsLifeLeft <= 0)
+            {
+                this.Enabled = false;
+                this.Visible = false;
+            }
+        }
+
+        public void Reset()
+        {
+            MsLifeLeft = OriginalLifeInMiliseconds;
+            Scale = BeginScale;
+            _scaleGrowthPerMilisecond = (EndScale - BeginScale) / MsLifeLeft;
+        }
+
+    }
+}
